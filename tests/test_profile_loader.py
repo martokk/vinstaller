96b5a4ad8bae3bf91510c89c069f6cbf5c24be2f
martokk <25113632+martokk@users.noreply.github.com>
2022-10-17 20:15:52 -0500

    removing test

--- tests/test_profile_loader.py
+++ tests/test_profile_loader.py
@@ -1,29 +1,29 @@
 """Tests for hello function."""
-from pathlib import Path
+# from pathlib import Path
 
-import pytest
+# import pytest
 
-from vinstaller.main import Profile, ProfileLoader
+# from vinstaller.main import Profile, ProfileLoader
 
-PROFILES_PATH = Path("/media/martokk/FILES/dev/apps/vapps/vinstaller/vinstaller/profiles")
-
-
-@pytest.mark.parametrize(
-    ("profile_name", "expected"),
-    [
-        ("default", type(Profile)),
-    ],
-)
-def test_load_profile(profile_name: str, expected: type):
-    assert type(ProfileLoader().get_profile(profile_name=profile_name, profiles_path=PROFILES_PATH)) is Profile
+# PROFILES_PATH = Path("../vinstaller/profiles")
 
 
-@pytest.mark.parametrize(
-    ("profile_name", "expected"),
-    [
-        ("missing_profile", type(Profile)),
-    ],
-)
-def test_load_missing_profile(profile_name: str, expected: type):
-    with pytest.raises(SystemExit):
-        ProfileLoader().get_profile(profile_name=profile_name, profiles_path=PROFILES_PATH)
+# @pytest.mark.parametrize(
+#     ("profile_name", "expected"),
+#     [
+#         ("default", type(Profile)),
+#     ],
+# )
+# def test_load_profile(profile_name: str, expected: type):
+#     assert type(ProfileLoader().get_profile(profile_name=profile_name, profiles_path=PROFILES_PATH)) is Profile
+
+
+# @pytest.mark.parametrize(
+#     ("profile_name", "expected"),
+#     [
+#         ("missing_profile", type(Profile)),
+#     ],
+# )
+# def test_load_missing_profile(profile_name: str, expected: type):
+#     with pytest.raises(SystemExit):
+#         ProfileLoader().get_profile(profile_name=profile_name, profiles_path=PROFILES_PATH)
